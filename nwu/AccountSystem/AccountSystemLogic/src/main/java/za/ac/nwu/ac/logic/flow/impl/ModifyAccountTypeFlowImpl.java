b923c4a355720efd3cf0260105bb69fb45d1c2fa
MarcellGreeff <marcellgreeff47@gmail.com>
2021-10-01 15:41:07 +0200

    Application Properties changed. Spring is running WITHOUT errors

--- nwu/AccountSystem/AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/ModifyAccountTypeFlowImpl.java
+++ nwu/AccountSystem/AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/ModifyAccountTypeFlowImpl.java
@@ -1,10 +1,14 @@
 package za.ac.nwu.ac.logic.flow.impl;
 
+import org.springframework.stereotype.Component;
 import za.ac.nwu.ac.domain.dto.AccountTypeDto;
 import za.ac.nwu.ac.logic.flow.ModifyAccountTypeFlow;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 
+@Transactional
+@Component("modifyAccountTypeFlowName")
 public class ModifyAccountTypeFlowImpl implements ModifyAccountTypeFlow {
 
     @Override
